21fdadce49ddba25b446a7ebb4ccf151eaa1f508
R-y-a-n8 <ryan.kinuthia@student.moringaschool.com>
2024-09-24 00:17:25 +0300

    compression

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(sequence):
+    """Return a list of all even elements in the given sequence of integers."""
+    return [n for n in sequence if n % 2 == 0]
 
-def make_exclamation(sentence_list):
-    pass
+def make_exclamation(sentences):
+    """Return a list of sentences with exclamation marks at the end."""
+    return [sentence + "!" for sentence in sentences]
